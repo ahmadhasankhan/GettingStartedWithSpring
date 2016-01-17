b2a7c281ce5ad7a756877b5a114096506c8ee6e1
Ahmad Hassan <er.ahmad.hassan@gmail.com>
2016-01-18 00:12:47 +0530

    first commit

---
+++ src/main/java/home/Application.java
@@ -0,0 +1,16 @@
+package home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by ahmad.hassan on 18/01/16.
+ */
+
+@SpringBootApplication
+public class Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+}
---
+++ src/main/java/home/WelcomeController.java
@@ -0,0 +1,22 @@
+package home;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by ahmad.hassan on 17/01/16.
+ */
+@Controller
+public class WelcomeController {
+
+  @RequestMapping("/")
+  public String home(
+      @RequestParam(value = "name", required = false, defaultValue = "Ahmad") String name,
+      Model model) {
+    model.addAttribute("name", name);
+    return "home";
+  }
+
+}
